0dafff869bf98537d478ad3abe968d3f619825ae
Sen <senjutipaul07@gmail.com>
2026-04-13 16:42:40 +0530

    added fuctionality

--- js.js
+++ js.js
@@ -2,34 +2,65 @@ let num = document.querySelectorAll(".number");
 let operators = document.querySelectorAll(".calc_operator");
 let clearButton = document.getElementById("AC/CE");
 let inp = document.getElementById("input");
-let numl1 = [];
-let fl1 = [];
-let fl2 = [];
-let numl2 = [];
-let opl = [];
+let scr= document.getElementById("screen");
 let l = [];
+var v = "";
 let result;
 
 calculate();
 function calculate() {
-    num.forEach((number) => {
-        number.addEventListener("click", () => {
-            var v = inp.value += number.innerText;
-            console.log(inp.value);
-            l.push(v);
-        });
-    });
     operators.forEach((operator) => {
         operator.addEventListener("click", () => {
-            var v = inp.value += operator.innerText;
-            console.log(inp.value);
-            if (v[v.length - 1] === "=") {
-                result = eval(inp.value.slice(0, -1));   
-                inp.value = result;
-                console.log(result);
+            c = operator.innerText;
+            scr.innerText+= c;
+            console.log(scr.innerText);
+            l.push(c);
+            console.log(l);
+            CE();
+        });
+    });
+    num.forEach((number) => {
+        number.addEventListener("click", () => {
+            n = number.innerText;
+            if (n != "=") {
+                scr.innerText += n;
+                l.push(n);
+                console.log(l);
+                CE();
             }
+            else {
+                console.log("ARRAY:", l);
+                result = eval(l.join(""));
+                // scr.innerText = "";
+                console.log(result);
+                scr.innerText = result;
+                AC(result);
+                l = [result];
+                
+            }
+
         });
 
     });
-    l = [];
+
+}
+
+function CE() {
+    if (scr.innerText != "") {
+        clearButton.innerText = "CE";
+        clearButton.onclick = () => {
+            scr.innerText = scr.innerText.slice(0, -1);
+            l.pop();
+        };
+    }
+}
+
+function AC(result) {
+    if (scr.innerText == result || scr.innerText == "" ) {
+        clearButton.innerText = "AC";
+        clearButton.onclick = () => {
+            scr.innerText = "";
+            l = [];
+        };
+    }
 }
